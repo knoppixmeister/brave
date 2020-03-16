c45f5f3e02c9a16b8a7358f7eb2c05d1f8e32e46
Adrian Cole <acole@pivotal.io>
2020-03-16 20:57:16 +0800

    WIP: tracecontext

--- instrumentation/benchmarks/src/main/java/brave/propagation/w3c/TraceContextPropagationBenchmarks.java
+++ instrumentation/benchmarks/src/main/java/brave/propagation/w3c/TraceContextPropagationBenchmarks.java
@@ -11,11 +11,14 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package brave.propagation;
+package brave.propagation.w3c;
 
 import brave.internal.HexCodec;
+import brave.propagation.Propagation;
+import brave.propagation.TraceContext;
 import brave.propagation.TraceContext.Extractor;
 import brave.propagation.TraceContext.Injector;
+import brave.propagation.TraceContextOrSamplingFlags;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -37,10 +40,11 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @Fork(3)
 @BenchmarkMode(Mode.SampleTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
-public class B3SinglePropagationBenchmarks {
-  static final Propagation<String> b3 = Propagation.B3_SINGLE_STRING;
-  static final Injector<Map<String, String>> b3Injector = b3.injector(Map::put);
-  static final Extractor<Map<String, String>> b3Extractor = b3.extractor(Map::get);
+public class TraceContextPropagationBenchmarks {
+  static final Propagation<String> tc =
+    TraceContextPropagation.FACTORY.create(Propagation.KeyFactory.STRING);
+  static final Injector<Map<String, String>> tcInjector = tc.injector(Map::put);
+  static final Extractor<Map<String, String>> tcExtractor = tc.extractor(Map::get);
 
   static final TraceContext context = TraceContext.newBuilder()
     .traceIdHigh(HexCodec.lowerHexToUnsignedLong("67891233abcdef01"))
@@ -49,33 +53,23 @@ public class B3SinglePropagationBenchmarks {
     .sampled(true)
     .build();
 
+  // TODO: add tracestate examples which prefer the b3 entry
   static final Map<String, String> incoming128 = new LinkedHashMap<String, String>() {
     {
-      put("b3", "67891233abcdef012345678912345678-463ac35c9f6413ad-1");
-    }
-  };
-
-  static final Map<String, String> incoming64 = new LinkedHashMap<String, String>() {
-    {
-      put("b3", "2345678912345678-463ac35c9f6413ad-1");
+      put("traceparent", TraceparentFormat.writeTraceparentFormat(context));
     }
   };
 
   static final Map<String, String> incomingPadded = new LinkedHashMap<String, String>() {
     {
-      put("b3", "00000000000000002345678912345678-463ac35c9f6413ad-1");
-    }
-  };
-
-  static final Map<String, String> incomingNotSampled = new LinkedHashMap<String, String>() {
-    {
-      put("b3", "0"); // unsampled
+      put("traceparent",
+        TraceparentFormat.writeTraceparentFormat(context.toBuilder().traceIdHigh(0).build()));
     }
   };
 
   static final Map<String, String> incomingMalformed = new LinkedHashMap<String, String>() {
     {
-      put("b3", "b970dafd-0d95-40aa-95d8-1d8725aebe40"); // not ok
+      put("traceparent", "b970dafd-0d95-40aa-95d8-1d8725aebe40"); // not ok
     }
   };
 
@@ -83,38 +77,30 @@ public class B3SinglePropagationBenchmarks {
 
   @Benchmark public void inject() {
     Map<String, String> carrier = new LinkedHashMap<>();
-    b3Injector.inject(context, carrier);
+    tcInjector.inject(context, carrier);
   }
 
   @Benchmark public TraceContextOrSamplingFlags extract_128() {
-    return b3Extractor.extract(incoming128);
-  }
-
-  @Benchmark public TraceContextOrSamplingFlags extract_64() {
-    return b3Extractor.extract(incoming64);
+    return tcExtractor.extract(incoming128);
   }
 
   @Benchmark public TraceContextOrSamplingFlags extract_padded() {
-    return b3Extractor.extract(incomingPadded);
+    return tcExtractor.extract(incomingPadded);
   }
 
   @Benchmark public TraceContextOrSamplingFlags extract_nothing() {
-    return b3Extractor.extract(nothingIncoming);
-  }
-
-  @Benchmark public TraceContextOrSamplingFlags extract_unsampled() {
-    return b3Extractor.extract(incomingNotSampled);
+    return tcExtractor.extract(nothingIncoming);
   }
 
   @Benchmark public TraceContextOrSamplingFlags extract_malformed() {
-    return b3Extractor.extract(incomingMalformed);
+    return tcExtractor.extract(incomingMalformed);
   }
 
   // Convenience main entry-point
   public static void main(String[] args) throws RunnerException {
     Options opt = new OptionsBuilder()
       .addProfiler("gc")
-      .include(".*" + B3SinglePropagationBenchmarks.class.getSimpleName())
+      .include(".*" + TraceContextPropagationBenchmarks.class.getSimpleName())
       .build();
 
     new Runner(opt).run();
